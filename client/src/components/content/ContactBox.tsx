6c36ccf23e9e9b1804d8f0ec5d4a63d3e0f0efb9
Jérémy Gendre <gendrejeremy@yahoo.fr>
2021-06-25 17:37:59 +0200

    divide in sub-components (contact / chat boxes)

---
+++ client/src/components/content/ContactBox.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import '../../style/contact.css';
+
+export default function ContactBox(){
+    return (
+        <div className="contact-box">
+            contacts
+        </div>
+    );
+}
--- client/src/components/content/Content.tsx
+++ client/src/components/content/Content.tsx
@@ -1,12 +1,15 @@
 import React from "react";
+import '../../style/content.css';
 import { io } from "socket.io-client";
+import ContactBox from "./ContactBox";
+import ChatBox from "./ChatBox";
 const socket = io();
-
 
 export default function Content(){
     return(
         <div className="app-content">
-
+            <ContactBox/>
+            <ChatBox/>
         </div>
     );
 }
