2f6c4cb300af3da34faea2b65438c7168b9c44cd
Jérémy Gendre <gendrejeremy@yahoo.fr>
2021-06-25 18:13:50 +0200

    style input bar

--- client/src/components/content/chat/InputBar.tsx
+++ client/src/components/content/chat/InputBar.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, {FormEvent} from "react";
 import '../../../style/chat/inputbar.css';
 
 export default function InputBar(){
+
+    const handleSubmit = (e:FormEvent) => {
+        e.preventDefault();
+        console.log('submitting');
+    };
+
     return(
-        <div className="input-bar-container">
-            <input type="text" placeholder="Enter your message..."/>
-        </div>
+        <form onSubmit={handleSubmit} className="input-bar-container">
+            <input type="text" placeholder="Enter your message..." required autoFocus/>
+            <button type="submit">Envoyer</button>
+        </form>
     );
 }
