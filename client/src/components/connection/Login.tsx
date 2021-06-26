31f54f2dd4581102b2a5257e574bd4876de630dd
Jérémy Gendre <gendrejeremy@yahoo.fr>
2021-06-26 19:59:08 +0200

    handle login form submission and log user

--- client/src/components/connection/Login.tsx
+++ client/src/components/connection/Login.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, {FormEvent, useContext, useState} from 'react';
 import '../../style/connection/login.css';
+import {UserContext} from "../../context/UserContext";
+import {getRandomInt} from "../../utils/main";
 
 export default function Login(){
+    const userContext = useContext(UserContext);
+    const [username, setUsername] = useState('');
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        const uid = getRandomInt(99999);
+        userContext.setUser({
+            username: username,
+            email: `${username}${uid}@gmail.com`,
+            name: 'Nom Test',
+            id: uid
+        });
+    };
+
     return(
         <div className="app-login-container">
             <div className="login-form-container">
-                <form className="login-form">
+                <form className="login-form" onSubmit={handleSubmit}>
                     <div className="login-form-row">Please, log in to use the chat</div>
                     <div className="login-form-row">
-                        <input type="text" required placeholder="Username"/>
+                        <input type="text" required placeholder="Username" value={username} onChange={e => {setUsername(e.target.value)}}/>
                     </div>
                     <div className="login-form-row">
                         <button type="submit">Login</button>
